bc6ddb4e9e62d7d53b65a7e515f57ff61a9ff3b7
MartinGC94 <MGC94@outlook.dk>
2024-11-25 02:43:21 +0100

    Make Enable-Display more consistent when enabling and disabling displays at the same time

--- src/DisplayConfig/Commands/EnableDisplayCommand.cs
+++ src/DisplayConfig/Commands/EnableDisplayCommand.cs
@@ -141,6 +141,7 @@ namespace MartinGC94.DisplayConfig.Commands
             {
                 if (error.NativeErrorCode == 31)
                 {
+                    WriteDebug("Failed to apply display config with standard approach. Retrying with alternative.");
                     // CCD sometimes fails to enable displays using the previous method so this is a fallback that uses a different approach
                     // The settings app shows similar behavior when enabling displays.
                     try
@@ -156,44 +157,96 @@ namespace MartinGC94.DisplayConfig.Commands
                 {
                     ThrowTerminatingError(new ErrorRecord(error, "FailedToApplyConfig", Utils.GetErrorCategory(error), null));
                 }
-
             }
         }
 
         private void RetryWithExistingConfig()
         {
+            // To disable displays we invalidate the source/target modeInfo indexes and remove the Path_Active flag
+            // To enable displays we invalidate the source/target modeInfo indexes, add the Path_Active flag
+            // we also add a unique clone group for each display (unless AsClone has been specified, which tells us to use one clone group for all the specified displays)
+            // Finally we ensure that there is a unique Id for each enabled display on each adapter.
+            // When enabling and disabling displays at the same time (for example due to a limited amount of supported outputs by the GPU)
+            // we need to reuse the IDs from the disabled displays for the newly added displays.
+            // Otherwise, we just need to ensure the IDs are all unique.
             var config = API.DisplayConfig.GetConfig();
+
+            var usedIds = new Dictionary<LUID, HashSet<uint>>();
+            var freedIds = new Dictionary<LUID, List<uint>>();
+            foreach (int pathIndex in displaysToDisable)
+            {
+                if (config.PathArray[pathIndex].flags.HasFlag(PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE))
+                {
+                    if (!freedIds.TryGetValue(config.PathArray[pathIndex].sourceInfo.adapterId, out List<uint> freeIdList))
+                    {
+                        freeIdList = new List<uint>();
+                        freedIds.Add(config.PathArray[pathIndex].sourceInfo.adapterId, freeIdList);
+                    }
+
+                    if (!usedIds.TryGetValue(config.PathArray[pathIndex].sourceInfo.adapterId, out HashSet<uint> usedIdSet))
+                    {
+                        usedIdSet = new HashSet<uint>();
+                        usedIds.Add(config.PathArray[pathIndex].sourceInfo.adapterId, usedIdSet);
+                    }
+
+                    freeIdList.Add(config.PathArray[pathIndex].sourceInfo.id);
+                    _ = usedIdSet.Add(config.PathArray[pathIndex].sourceInfo.id);
+                }
+
+                config.PathArray[pathIndex].sourceInfo.modeInfoIdx = API.DisplayConfig.DISPLAYCONFIG_PATH_MODE_IDX_INVALID;
+                config.PathArray[pathIndex].targetInfo.modeInfoIdx = API.DisplayConfig.DISPLAYCONFIG_PATH_MODE_IDX_INVALID;
+                config.PathArray[pathIndex].flags &= ~PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE;
+            }
+
+            foreach (int index in config.AvailablePathIndexes)
+            {
+                if (config.PathArray[index].flags.HasFlag(PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE))
+                {
+                    if (!usedIds.TryGetValue(config.PathArray[index].sourceInfo.adapterId, out HashSet<uint> usedIdSet))
+                    {
+                        usedIdSet = new HashSet<uint>();
+                        usedIds.Add(config.PathArray[index].sourceInfo.adapterId, usedIdSet);
+                    }
+
+                    _ = usedIdSet.Add(config.PathArray[index].sourceInfo.id);
+                }
+            }
+
             uint cloneGroupToSet = 0;
             foreach (int pathIndex in displaysToEnable)
             {
+                if (!config.PathArray[pathIndex].flags.HasFlag(PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE))
+                {
+                    uint newId;
+                    if (freedIds.TryGetValue(config.PathArray[pathIndex].sourceInfo.adapterId, out List<uint> ids) && ids.Count > 0)
+                    {
+                        newId = ids[ids.Count - 1];
+                        ids.RemoveAt(ids.Count - 1);
+                    }
+                    else
+                    {
+                        if (!usedIds.TryGetValue(config.PathArray[pathIndex].sourceInfo.adapterId, out HashSet<uint> usedIdSet))
+                        {
+                            usedIdSet = new HashSet<uint>();
+                            usedIds.Add(config.PathArray[pathIndex].sourceInfo.adapterId, usedIdSet);
+                        }
+
+                        newId = 0;
+                        while (!usedIdSet.Add(newId))
+                        {
+                            newId++;
+                        }
+                    }
+
+                    config.PathArray[pathIndex].sourceInfo.id = newId;
+                }
+
                 config.PathArray[pathIndex].targetInfo.modeInfoIdx = API.DisplayConfig.DISPLAYCONFIG_PATH_MODE_IDX_INVALID;
                 config.PathArray[pathIndex].sourceInfo.ResetModeAndSetCloneGroup(cloneGroupToSet);
                 config.PathArray[pathIndex].flags |= PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE;
                 if (!AsClone)
                 {
                     cloneGroupToSet++;
-                }
-            }
-
-            foreach (int pathIndex in displaysToDisable)
-            {
-                config.PathArray[pathIndex].sourceInfo.modeInfoIdx = API.DisplayConfig.DISPLAYCONFIG_PATH_MODE_IDX_INVALID;
-                config.PathArray[pathIndex].targetInfo.modeInfoIdx = API.DisplayConfig.DISPLAYCONFIG_PATH_MODE_IDX_INVALID;
-                config.PathArray[pathIndex].flags &= ~PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE;
-            }
-
-            var sourceIdTable = new Dictionary<LUID, uint>();
-            foreach (int index in config.AvailablePathIndexes)
-            {
-                if (config.PathArray[index].flags.HasFlag(PathInfoFlags.DISPLAYCONFIG_PATH_ACTIVE))
-                {
-                    LUID adapterId = config.PathArray[index].sourceInfo.adapterId;
-                    if (!sourceIdTable.ContainsKey(adapterId))
-                    {
-                        sourceIdTable.Add(adapterId, 0);
-                    }
-
-                    config.PathArray[index].sourceInfo.id = sourceIdTable[adapterId]++;
                 }
             }
 
